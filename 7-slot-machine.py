d7ed851975c8020692451fd7e3ae42ccbb933b51
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-05-22 23:33:50 +0300

    test: we tested with different scenarios and fixed the code accordingly

--- 7-slot-machine.py
+++ 7-slot-machine.py
@@ -42,17 +42,19 @@ def play(bet, balance) -> int:
     """
 
     balance -= bet
-    choices = ["orange", "apple", "cherry", "bar", "heart", "diamond"]
+    choices = ["apple", "orange", "bar", "seven" "heart", "diamond"]
     slot1 = random.choice(choices)
     slot2 = random.choice(choices)
     slot3 = random.choice(choices)
     
-    print(f"{slot1} {slot2} {slot3}")
+    print("---------------------------")
+    print(f"| {slot1} | {slot2} | {slot3} |")
+    print("---------------------------")
     
     if slot1 == slot2 == slot3:
         for idx in range(6):
             if slot1 == choices[idx]:
-                winnings = bet * (idx + 1)
+                winnings = bet * ((idx + 1) * 2)
                 balance += winnings 
         print(f"Congrats you have won ${winnings} your balance is ${balance}")
     else:
@@ -69,34 +71,38 @@ def gambling() -> int:
         int: The final balance after the player decides to cash out.
     """
     balance = money()
+    count = 0
 
-    while balance > 0:
-        gamble = input("Press Enter if you want to keep playing, N if you want to cash out (Enter / N): ")
-        if gamble.lower() == "n":
-            return balance
-        betting = input("Please insert your bet?  ")
-        if betting.isdigit():
-            bet = int(betting)
+    while balance >= 0:
+        bet = 0
+        if count > 0:
+            gamble = input("Press Enter if you want to play, N if you want to cash out (Enter / N): ")
+            if gamble.lower() == "n":
+                return balance
         else:
-            print("Please enter a valid number")
-            continue
-        if bet < balance:
-            balance = play(bet, balance)
+            count += 1
+        if balance > 0:
+            betting = input("Please insert your bet? $ ")
+            if betting.isdigit() and int(betting) > 0:
+                bet = int(betting)
+            else:
+                print("Please enter a valid number")
+                continue
+            if bet <= balance:
+                balance = play(bet, balance)
+            else:
+                print("You do not have enough money to make that bet")
         else:
-            print("You do not have enough money to make that bet")
+            if bet > 0:
+                print("You do not have enough money to make that bet")
             deposit = input("would you like to make a deposit? (Y/N): ")
             if deposit.lower() == "y":
                 balance += money()
             else:   
                 continue
-        
+
     return balance
 
 
 if __name__ == "__main__":
     print(gambling())
-
-
-
-
-
